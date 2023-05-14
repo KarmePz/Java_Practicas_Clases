6cd4d9c4e5004f659639cf781d31c09ff5e3d17e
KarmePz <baezjulicarme16@gmail.com>
2023-05-14 03:38:52 -0300

    Se termino ejercicio 6 Condicionales

--- Java_practicas_de_clase/src/Estructuras_condicionales/src/Est_Con_Ejercicio6.java
+++ Java_practicas_de_clase/src/Estructuras_condicionales/src/Est_Con_Ejercicio6.java
@@ -1,9 +1,23 @@
 package Estructuras_condicionales.src;
-import java.text.DecimalFormat;
 import java.util.Scanner;
 public class Est_Con_Ejercicio6 {
     public static void main(String[] args){
-
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un numero del 1 al 10: ");
+        int a;
+        a = sc.nextInt();
+        switch (a){
+            case 1: System.out.println("Número 1 en romano: I");break;
+            case 2: System.out.println("Número 2 en romano: II");break;
+            case 3: System.out.println("Número 3 en romano: III");break;
+            case 4: System.out.println("Número 4 en romano: IV");break;
+            case 5: System.out.println("Número 5 en romano: V");break;
+            case 6: System.out.println("Número 6 en romano: VI");break;
+            case 7: System.out.println("Número 7 en romano: VII");break;
+            case 8: System.out.println("Número 8 en romano: VIII");break;
+            case 9: System.out.println("Número 9 en romano: IX");break;
+            case 10: System.out.println("Número 10 en romano: X");break;
+            default: System.out.println("Número fuera de rango");break;
+        }
     }
 }

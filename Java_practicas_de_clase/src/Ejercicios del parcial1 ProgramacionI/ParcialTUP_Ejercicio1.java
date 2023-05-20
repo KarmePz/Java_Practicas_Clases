4370de7bcb5dbe7a3b7ca079096a2332cbbf9684
KarmePz <baezjulicarme16@gmail.com>
2023-05-20 16:49:42 -0300

    Se restauraron archivos

---
+++ Java_practicas_de_clase/src/Ejercicios del parcial1 ProgramacionI/ParcialTUP_Ejercicio1.java
@@ -0,0 +1,24 @@
+
+//Alumno: BAEZ CARMELO JOSE JULIAN
+
+
+
+//Dado los estados iniciales y finales, escribir las acciones:
+//estados iniciales: a = 3; b = 10; P = ?
+//estados finales: a = 7; b = 10; P = 21
+
+public class ParcialTUP_Ejercicio1 {
+    public static void main(String[] args){
+        int a = 3,b = 10, P;//Iniciamos P sin "igual" ya que no sabemos cuanto vale en su estao inicial
+        //se muestran los valores por pantalla
+        System.out.println("Los valores iniciales son: A= "+a+ " B = "+ b+" P = ?");
+
+        //acciones:
+            P = b-a;
+            P = P*a;
+            a = b - a;
+
+        //Se muestran resultados por pantalla
+        System.out.println("Los valores finales son: A= "+a+ " B = "+ b+" P = "+P);
+    }
+}

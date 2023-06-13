89a94578468653ac90f9ab6dfaeaa7ee1e781865
KarmePz <baezjulicarme16@gmail.com>
2023-06-13 11:24:19 -0300

    Se comento codigo

--- Java_practicas_de_clase/src/Parcialteorico2/POO_BaezCarmelo/src/Ejercicio2_38_BaezCarmelo/Libro.java
+++ Java_practicas_de_clase/src/Parcialteorico2/POO_BaezCarmelo/src/Ejercicio2_38_BaezCarmelo/Libro.java
@@ -1,20 +1,23 @@
 package Ejercicio2_38_BaezCarmelo;
 
 public class Libro extends Object{
-    //atributos
+                                        //ATRIBUTOS
     private String titulo;
     private String autor;
     private int numejemplares;
     private int numejemplaresprestados;
 
-    //Metodos
-    public Libro() { //Constructor por defecto
+                                        //METODOS
+
+                    //Constructor por defecto
+    public Libro() {
         this.titulo = "No especificado";
         this.autor = "No especificado";
         this.numejemplares = 1;
         this.numejemplaresprestados = 0;
     }
-    public Libro(String titulo, String autor, int numejemplares, int numejemplaresprestados){//Constructor por parametros
+                    //Constructor con parametros
+    public Libro(String titulo, String autor, int numejemplares, int numejemplaresprestados){
         this.titulo = titulo;
         this.autor = autor;
         this.numejemplares = numejemplares;
@@ -26,7 +29,7 @@ public class Libro extends Object{
 
     }
 
-    //metodos getters
+                    //metodos getters
     public int getNumejemplares(){
         return numejemplares;
     }
@@ -40,7 +43,7 @@ public class Libro extends Object{
         return autor;
     }
 
-    //metodos setters
+                    //metodos setters
     public void setNumejemplares(int numejemplares){
         this.numejemplares = numejemplares;
     }
@@ -54,28 +57,31 @@ public class Libro extends Object{
         this.autor = autor;
     }
 
+                    //Metodo Prestamo
     public boolean prestamo (int librosaprestar) {
         int numejemplaresdisponibles = numejemplares-numejemplaresprestados;
         if(numejemplaresdisponibles == 0 || librosaprestar>numejemplaresdisponibles || librosaprestar<0){
-            return false; //no quedan ejemplares a prestar o no se pueden prestar
+            return false; //no quedan ejemplares a prestar/no se pueden prestar ejemplares que superen los disponibles /el rango ingresado es invalido
         }else{
             numejemplaresprestados = numejemplaresprestados+librosaprestar;//se modifica la cantidad de ejemplares prestados,
         return true;}
     }
+                    //Metodo Devolucion
     public boolean devolucion(int librosdevueltos){
         if(numejemplaresprestados == 0 || librosdevueltos <= 0 || librosdevueltos > numejemplaresprestados){
-            return false; //no se pueden devolver ejemplares, No se prestaron, el dato es invalido o se devolvieron mas de los que se prestaron.
+            return false; //no se pueden devolver ejemplares/No se prestaron/el dato es invalido/ se devolvieron mas ejemplares de los que se prestaron.
         }else{
             numejemplaresprestados = numejemplaresprestados-librosdevueltos;//se modifica la cantidad de ejemplares prestados,
             return true;}
     }
 
+                    //Metodo heredado toString
     @Override
     public String toString(){
         String datos = "El titulo del libro es: " + titulo + "\n" +
                        "El autor del libro es: " + autor +"\n"+
                        "El número de ejemplares total del libro es: " + numejemplares + "\n"+
                        "El número de ejemplares prestados es "+numejemplaresprestados;
-        return datos;
+        return datos;//Se retorna la cadena "datos" con todos los datos correspondientes.
     }
 }
--- Java_practicas_de_clase/src/Parcialteorico2/POO_BaezCarmelo/src/Ejercicio2_38_BaezCarmelo/pruebaLibro.java
+++ Java_practicas_de_clase/src/Parcialteorico2/POO_BaezCarmelo/src/Ejercicio2_38_BaezCarmelo/pruebaLibro.java
@@ -1,6 +1,6 @@
 package Ejercicio2_38_BaezCarmelo;
 
-import Ejercicio2_38_BaezCarmelo.Libro;
+/*import Ejercicio2_38_BaezCarmelo.Libro;*/
 
 import java.util.Scanner;
 
@@ -8,12 +8,17 @@ public class pruebaLibro {
     public static void main (String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        Libro libro1 = new Libro("La ballena roja", "Mariano Schecopar", 8, 0);
+        System.out.println("\n************Prueba de Instancia con y sin parametros especificados*********************");
+
+                                        //Metodo constructor por parametros:
+        Libro libro1 = new Libro("La ballena roja", "Mariano Schecopar", 8, 3);
         System.out.println("\nEl libro 1 tiene los siguientes datos: \n"+libro1.toString());
 
+                                        //Metodo constructor por defecto
         Libro libro2 = new Libro();
         System.out.println("\nEl libro 2 tiene los siguientes datos: \n " + libro2.toString());
 
+                                        //Prueba de Setters
         System.out.println("\nIngrese los datos del libro2 (titulo)(autor)(ejemplares totales)(ejemplares prestados): ");
         String nombre = sc.nextLine();String autor = sc.nextLine();int ejemplares = sc.nextInt();int ejemplaresP = sc.nextInt();
         libro2.setTitulo(nombre);
@@ -25,20 +30,33 @@ public class pruebaLibro {
             libro2.setNumejemplaresprestados(ejemplaresP);
         }
 
-        boolean prestamo_libro1 = libro1.prestamo(4);
-        if (prestamo_libro1 == true) {
-            System.out.println("\nSe prestaron de unos  " + libro1.getNumejemplaresprestados() + " libros de titulo '"+libro1.getTitulo()+"' en total");
+
+        System.out.println("\n*****************Prueba de metodo PRESTAMO ********************************************");
+                                        //Prueba de metodo prestamo y getters
+        System.out.println("Ingrese la cantidad de ejemplares a prestar del libro "+libro1.getTitulo()+" :");
+        int ejemplaresP2 = sc.nextInt();
+
+        boolean prestamo_libro1 = libro1.prestamo(ejemplaresP2);
+        if (prestamo_libro1) {
+            System.out.println("\nSe prestaron unos  " + libro1.getNumejemplaresprestados() + " libros de titulo '"+libro1.getTitulo()+"' en total");
         } else {
-            System.out.println("\nNo se puede prestar nada");
+            System.out.println("\nNo se efectuo prestamo");
         }
 
-        boolean devolucion_libro1 = libro1.devolucion(3);
-        if (devolucion_libro1 == true) {
-            System.out.println("\nSe devolvieron libros. El total de ejemplares prestados es ahora de  " + libro1.getNumejemplaresprestados() + " libros de titulo '"+libro1.getTitulo()+"' en total");
+        System.out.println("\n*****************Prueba de metodo DEVOLUCION*******************************************");
+                                        //Prueba de metodo devolucion y getters
+        //ingreso de datos
+        System.out.println("Ingrese la cantidad de ejemplares a devolver del libro "+libro1.getTitulo()+" :");
+        int ejemplaresD = sc.nextInt();
+
+        boolean devolucion_libro1 = libro1.devolucion(ejemplaresD);
+        if (devolucion_libro1) {
+            System.out.println("\nSe devolvieron libros. El total de ejemplares prestados es ahora de  " + libro1.getNumejemplaresprestados() + " libros de titulo '"+libro1.getTitulo()+".");
         } else {
             System.out.println("\nNo se efectuo la devolucion correctamente");
         }
 
+        System.out.println("\n*****************Prueba de metodo heredado toString()**********************************");
         System.out.println("\nEl libro 1 tiene los siguientes datos: \n"+libro1.toString());
         System.out.println("\nEl libro 2 tiene los siguientes datos: \n "+libro2.toString());
 
